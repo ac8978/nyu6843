f0338cee3a34f2085b22d5ca44bc940968077f6a
ac8978 <78660114+ac8978@users.noreply.github.com>
2021-02-25 16:33:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    msg = "\r\n My message"
    endmsg = "\r\n.\r\n"
 
-   # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
-   # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
    # Fill in start
    clientSocket = socket(AF_INET, SOCK_STREAM)
    ms = (mailserver, port)
@@ -16,18 +12,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    # Fill in end
 
    recv = clientSocket.recv(1024).decode()
-   #print(recv)
-   #if recv[:3] != '220':
-       #print('220 reply not received from server.')
 
-   # Send HELO command and print server response.
    heloCommand = 'HELO Alice\r\n'
    clientSocket.send(heloCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
-   #print(recv1)
-   #if recv1[:3] != '250':
-       #print('250 reply not received from server.')
-
+   
    # Send MAIL FROM command and print server response.
    # Fill in start
    clientSocket.send(("MAIL FROM: <ac8978@nyu.edu>\r\n").encode())
